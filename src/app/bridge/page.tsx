78bf09ee3dec9c1270b8b6a7c219a473f3f315f8
emadhamad171 <dusikeh@gmail.com>
2025-12-24 15:20:47 +0200

    add bridge stats section

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -1,6 +1,6 @@
-import {Footer, Navigation} from '@/components'
 import BridgeHero from '@/components/BridgeHero'
 import BridgeNavigation from '@/components/BridgeNavigation'
+import BridgeStats from '@/components/BridgeStats'
 import React from 'react'
 
 export default function BridgePage() {
@@ -8,6 +8,7 @@ export default function BridgePage() {
     <div className="min-h-screen bg-[#D4D4D4] text-black">
       <BridgeNavigation/>
       <BridgeHero/>
+      <BridgeStats/>
     </div>
   )
 }
---
+++ src/components/BridgeStats.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Image from "next/image";
+
+const stats = [
+    { value: "8+", label: "chains supported" },
+    { value: "<$0.01", label: "average fee" },
+    { value: "<1", label: "second settlement" },
+    { value: "$XX", label: "USDT bridged" }
+  ];
+
+const duplicatedStats = [...stats, ...stats, ...stats];
+
+const BridgeStats = () => {
+  return (
+    <section className=" overflow-visible bg-[#D4D4D4]">
+      {/* rails like the landing page */}
+      <div className="relative mx-auto w-22/23 sm:w-[640px] md:w-11/12 border-[#C6C6C6] border-l border-r border-b">
+          <div
+            className="hidden lg:block absolute pointer-events-none -top-18 -right-7.5 z-10"
+            style={{ filter: "drop-shadow(0 20px 30px rgba(0,0,0,0.40))" }}
+          >
+            <Image
+                src="/Group39.webp"
+                alt="3D Coin Element"
+                width={150}
+                height={150}
+                className="w-20 sm:w-28 lg:w-32 h-auto"
+              />
+          </div>
+
+          <div className="w-full overflow-hidden">
+            <div className="relative">
+              <div className="flex animate-slide">
+                {duplicatedStats.map((stat, index) => (
+                  <div 
+                    key={index} 
+                    className={` shrink-0 w-[350px] lg:w-[451px] pl-7.5 lg:pl-[60px] py-5 lg:py-7 ${index % 2 != 0 ? "bg-[#E2E2E2]" : ''}`}
+                  >
+                    <div className="leading-tight text-[70px] lg:text-[100px] font-medium text-medium mb-1">
+                      {stat.value}
+                    </div>
+                    <div className="text-lg opacity-50">
+                      {stat.label}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  );
+};
+
+export default BridgeStats;
